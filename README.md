2f8aeb9e5b43f39f5f784ae1da1fc91d54d20d22
meenashi710 <meenashi.t.arunachalam@gmail.com>
2024-07-07 20:08:44 +0530

    Testing the commit and push

---
+++ README.md
@@ -0,0 +1,4 @@
+# Hello All
+This is Java Coding Learning repo/n
+
+Just have a look at my progress
--- src/ex_07072024/Lab001.java
+++ src/ex_07072024/Lab001.java
@@ -1,2 +1,10 @@
-package ex_07072024;public class Lab001 {
+package ex_07072024;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(2+2);
+    }
 }
